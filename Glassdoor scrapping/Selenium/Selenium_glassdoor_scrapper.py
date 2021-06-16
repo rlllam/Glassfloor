ccb9443d16f906bd0b8ae86252a4c208a305d66c
Dellis <dellis.ng@mail.utoronto.ca>
2021-06-16 08:16:26 -0400

    Stablized Selenium_glassdoor_scrapper.py

--- Glassdoor scrapping/Selenium/Selenium_glassdoor_scrapper.py
+++ Glassdoor scrapping/Selenium/Selenium_glassdoor_scrapper.py
@@ -6,8 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-start_time = time.time()
-
 # ChromeDriver 90.0.4430.24
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -25,37 +23,26 @@ JOB_DESC_CONTAINER_ID = "JobDescriptionContainer"
 
 SHOW_MORE_XPATH = "//*[@id='JobDescriptionContainer']/div[2]"
 
-NEXT_PAGE_XPATH = "//*[@id='FooterPageNav']/div/ul/li[7]/a"
+PAGE_BUTTON_XPATH_COMMON_SYNTAX = "//*[@id='FooterPageNav']/div/ul/li"
 
 # Set webdriver to chrome
 driver = webdriver.Chrome(PATH)
-
+driver.minimize_window()
 
 def check_exists_by_xpath(xpath):
     """
     Check if an element exists by xpath.
     """
-
     try:
         driver.find_element_by_xpath(xpath)
     except exceptions.NoSuchElementException:
         return False
     return True
 
-def click_next_page():
-    """
-    Clicks the next page button
-    """
-
-    next_page_button = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, NEXT_PAGE_XPATH)))
-    next_page_button.click()
-
 def close_sign_up():
     """
     Ensures the sign up prompt is closed.
     """
-
     while check_exists_by_xpath(SIGN_UP_PROMPT_XPATH):
         # Press 'Esc' globally
         webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
@@ -63,7 +50,7 @@ def close_sign_up():
 def instantiate_show_more():
     """
     Instantiate reference to the show more button and store it into 'show more'.
-    Repeat indefinitely until it works.
+    Retry indefinitely until it works.
     """
     instantiate_successfully = False
 
@@ -101,7 +88,6 @@ def scrap_job_desc(job_box):
     """
     Returns the job description of the job box as a string.
     """
-
     job_box.click()
 
     click_show_more()
@@ -116,31 +102,64 @@ def scrap_job_desc(job_box):
         except exceptions.StaleElementReferenceException:
             pass
 
+def go_to_next_page():
+    """
+    Ensures the next page is reached
+    """
+    while True:
+        try:
+            page_list = WebDriverWait(driver, 30).until(
+                EC.presence_of_all_elements_located((By.XPATH, PAGE_BUTTON_XPATH_COMMON_SYNTAX)))
+
+            # The Xpath of the next page button is //*[@id='FooterPageNav']/div/ul/li[*last index*]
+            next_page_button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, PAGE_BUTTON_XPATH_COMMON_SYNTAX + "[" + str(len(page_list)) + "]")))
+
+            button_is_disabled = next_page_button.get_attribute("disabled")
+
+            # Already reached the last page
+            if button_is_disabled:
+                return False
+
+            old_url = driver.current_url
+            next_page_button.click()
+
+            # Ensuring the next page is reached
+            while driver.current_url == old_url:
+                continue
+
+            return True
+
+        # If there is an exception, refresh the current page and retry
+        except:
+            driver.get(driver.current_url)
+
 def scrap_page():
     """
     Returns all job descriptions on the current page as an array of strings.
     """
-
-    # WebDriver will wait 20 seconds until the job box list is found and store it
-    job_box_list = WebDriverWait(driver, 20).until(
-        EC.presence_of_all_elements_located((By.XPATH, JOB_BOX_COMMON_XPATH_SYNTAX)))
-
-    # Put all job descriptions into 'scrapped_page'
-    scrapped_page = []
-    for idx in range(len(job_box_list)):
-        # We want to ensure the job box element is found and stored into job_box
+    while True:
         try:
-            # If the job box is not found within 5 seconds, the driver will click the next page button call this function again
-            job_box = WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable((By.XPATH, JOB_BOX_COMMON_XPATH_SYNTAX+ "[" + str(idx+1) + "]")))
-            
-        # If there is a time out exception, it means the next page was not loaded
-        except exceptions.TimeoutException:
-            click_next_page()
-            return scrap_page()
+            # WebDriver will wait 30 seconds until the job box list is found and store it
+            job_box_list = WebDriverWait(driver, 30).until(
+                EC.presence_of_all_elements_located((By.XPATH, JOB_BOX_COMMON_XPATH_SYNTAX)))
 
-        scrapped_page.append(scrap_job_desc(job_box))
-        print("Jobs scrapped: " + str(idx+1))
+            # Put all job descriptions into 'scrapped_page'
+            scrapped_page = []
+
+            for idx in range(len(job_box_list)):
+                job_box = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.XPATH, JOB_BOX_COMMON_XPATH_SYNTAX+ "[" + str(idx+1) + "]")))
+
+                scrapped_page.append(scrap_job_desc(job_box))
+                print("Jobs scrapped: " + str(idx+1))
+
+            break
+
+        # If there is an exception, refresh the current page and retry
+        except:
+            driver.get(driver.current_url)
+    
 
     return scrapped_page
 
@@ -148,6 +167,8 @@ def scrap(keyword, location, pages_to_scape=1):
     """
     Returns an array of job descriptions as strings.
     """
+    print("Starting...")
+    start_time = time.time()
 
     # Go on the desired glassdoor search results page
     driver.get("https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword="
@@ -164,8 +185,13 @@ def scrap(keyword, location, pages_to_scape=1):
         pages_scaped = i+1
 
         if pages_scaped < pages_to_scape:
-            # WebDriver will wait 20 seconds until the next page button is clickable and store it
-            click_next_page()
+            page_exist = go_to_next_page()
+            if not page_exist:
+                break
+
+    # Record time it took to run scrap()
+    time_taken = time.time() - start_time
+    print("\n" + str(len(scrapped_job_descriptions)) + " entries in total\nTime Taken: " + str(time_taken))
 
     return scrapped_job_descriptions
 
@@ -178,11 +204,6 @@ def scrap(keyword, location, pages_to_scape=1):
 scrapped_desc = scrap('software', 'canada', 10)
 driver.quit()
 
-# Record time it took to run scrap()
-time_taken = time.time() - start_time
-
 # Format the output and print it
-for idx, job_desc in enumerate(scrapped_desc):
-        print(str(idx+1) + ". " + job_desc + "\n")
-
-print(str(len(scrapped_desc)) + " entries in total\nTime Taken: " + str(time_taken))
+# for idx, job_desc in enumerate(scrapped_desc):
+#         print(str(idx+1) + ". " + job_desc + "\n")
---
+++ Glassdoor scrapping/Selenium/stability_test.py
@@ -0,0 +1,8 @@
+# import os
+# from subprocess import *
+from subprocess import Popen
+
+# Use a VPN for this because Glassdoor will probably block your ip after testing for a while
+
+for i in range(10):
+	Popen("Selenium_glassdoor_scrapper.py", shell=True)
